0d092b8e091cb94b9c6fe0be6748890abceecaa1
Daniele Vaccari <djvacc@utu.fi>
2021-01-30 14:41:08 +0200

    Initial Commit

---
+++ sand/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sand
+//
+//  Created by Daniele Johannes Vaccari on 30.1.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
